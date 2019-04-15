7e6ca4b5dc92bd2c9891de0bcc00420dabe72e2c
Kate F <follett.katie@gmail.com>
2019-04-15 03:16:39 +0000

    Automatically backed up by Learn

---
+++ lib/post.rb
@@ -0,0 +1,5 @@
+class Post
+  #should belong to an author.
+  #should have a title .
+  #should belong to an author.
+end
---
+++ lib/song.rb
@@ -0,0 +1,6 @@
+class Song
+  #should belong to an artist.
+  #should have a title.
+  #should be able to tell you the name of the artist.
+  
+end
